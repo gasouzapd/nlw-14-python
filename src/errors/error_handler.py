ff97dba519d6a4ce2ba37713468456e117582ab5
Gabriel P <gasouzapd@gmail.com>
2024-02-08 23:11:23 -0300

    Implementando validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,20 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        # enviar para um log
+        # enviar um email de notificao
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+
     return HttpResponse(
         status_code=500,
         body={
@@ -10,4 +24,3 @@ def handle_errors(error: Exception) -> HttpResponse:
             }]
         }
     )
-
